586a288f404de78f0eee2fc0976618143a6ed100
Dhia M <merzougui.dhia@gmail.com>
2022-01-19 21:55:50 +0100

    Fixed tokenization bug, fixed andd improved filtering functions.

--- summariser.js
+++ summariser.js
@@ -8,36 +8,51 @@ We’ll still have to wait for consumer TVs to actually make it to market using 
 The question still remains when, exactly, Samsung Display’s new panels will actually go on sale. Interestingly the first QD-OLED TV to be announced wasn’t from Samsung Electronics, but was instead from Sony which said its Bravia XR A95K will use a QD-OLED panel from Samsung Display. Alienware also has a QD-OLED computer monitor in the works. When Samsung Electronics will eventually release a QD-OLED TV of its own is anyone’s guess.
 `;
 
+const filterWord = (word) => {
+
+    // Filters a word out of punctuation and spaces
+
+    let result = word;
+    let punctuation = [".", ",", "!", "?", ";"];
+
+    if(punctuation.includes(word.at(-1) || word.at(-1) == " ")){
+        result = word.slice(0, -1);
+    }
+
+    return result;
+}
+
+const filterText = (text) => {
+
+    // Filters text of unwanted words, punctuation and spaces.
+
+    let notMeaningful = ["And", "and", "The", "the", "Or", "or", "Is", "is", "Of", "of", "A", "a", "to", "To", "be", "Be", "Of", "of", "Which", "which", "it", "It", "is", "Is", "has", "Has", "not", "Not", "In", "in", "For", "for", "no", "No", "than", "Than", "are", "Are"];
+
+    for(let i = text.length - 1; i >= 0 ; i--){
+        if(notMeaningful.includes(text[i])){
+            text.splice(i, 1);
+        }
+
+        else{
+            text[i] = filterWord(text[i]);
+        }
+    }
+}
+
+const filterSentences = (sentenceTokens) =>{
+
+    // Filters sentences out of things that might make the words uncreognisable by the wordsMap.
+
+    for(let i = 0; i < sentenceTokens.length; i++){
+        filterText(sentenceTokens[i]);
+    }
+}
+
 const tokenizeWords = (text) => {
 
     // Returns a list of all words in a given string
 
     return text.split(" ");
-}
-
-const filterText = (text) => {
-
-    // Filters text of unwanted words.
-    // == PROBLEM: Only deletes the first occurence of an unwanted word. == 
-
-    let notMeaningful = ["And", "and", "The", "the", "Or", "or", "Is", "is", "Of", "of", "A", "a", "to", "To", "be", "Be", "Of", "of", "Which", "which", "it", "It", "is"];
-    let punctuation = [".", ",", "!", "?", ";"];
-
-    for(let i = 0; i < text.length; i++){
-        if(notMeaningful.includes(text[i])){
-            // text.splice(i, 1);
-            // This possibly fixes the problem
-            text[i] = "";
-        }
-
-        if(punctuation.includes(text[i].at(-1))){
-            text[i] = text[i].substr(0, text[i].length - 1);
-        }
-
-        if(text[i].at(-1) == " "){
-            text[i] = text[i].substr(0, text[i].length - 1);
-        }
-    }
 }
 
 const tokenizeSentences = (text) => {
@@ -57,7 +72,7 @@ const tokenizeSentences = (text) => {
         }
 
         if(c == "."){
-            // filterText(sentence);
+            sentence.push(word);
             sentences.push(sentence);
             sentence = [];
             word = "";
@@ -65,13 +80,6 @@ const tokenizeSentences = (text) => {
     }
     
     return sentences;
-}
-
-const filterSentences = (sentenceTokens) =>{
-    for(let i = 0; i < sentenceTokens.length; i++){
-        // console.log("========= SENTENCE FILTER =========\n");
-        filterText(sentenceTokens[i]);
-    }
 }
 
 const getWordsMap = (tokenizedWords) =>{
@@ -186,47 +194,46 @@ const summarise = (text) =>{
 
     for(sentence in sentenceMap){
         if(sentenceMap[sentence] >= averageWeight){
-            result += "\n";
             result += sentence;
+            result += "\n";
         }
     }
 
     return result;
-
 }
 
-// // ======== Tests ========
+// ======== Tests ========
 
-// // ==== Word Tests ====
-// let wordTokens = tokenizeWords(test_text);
+// ==== Word Tests ====
+let wordTokens = tokenizeWords(test_text);
 
-// // Filtering Test:
-// // console.log(wordTokens);
-// // console.log("Not filtered size: " + wordTokens.length);
-// filterText(wordTokens);
-// // console.log(wordTokens);
-// // console.log("Filtered size: " + wordTokens.length);
+// Filtering Test:
+// console.log(wordTokens);
+// console.log("Not filtered size: " + wordTokens.length);
+filterText(wordTokens);
+// console.log(wordTokens);
+// console.log("Filtered size: " + wordTokens.length);
 
-// // Map test:
-// let wordsMap = getWordsMap(wordTokens);
-// // console.log(wordsMap);
+// Map test:
+let wordsMap = getWordsMap(wordTokens);
+// console.log(wordsMap);
 
-// // Weigh Test:
-// weighWords(wordsMap);
-// // console.log(wordsMap);
+// Weigh Test:
+weighWords(wordsMap);
+// console.log(wordsMap);
 
-// // ==== Sentence Tests ====
-// let sentenceTokens = tokenizeSentences(test_text);
+// ==== Sentence Tests ====
+let sentenceTokens = tokenizeSentences(test_text);
 // filterSentences(sentenceTokens);
-// // console.log(sentenceTokens);
+// console.log(sentenceTokens);
 
-// // // Scoring Test:
-// let sentenceMap = scoreSentences(sentenceTokens, wordsMap);
-// // console.log(sentenceMap);
+// Scoring Test:
+let sentenceMap = scoreSentences(sentenceTokens, wordsMap);
+// console.log(sentenceMap);
 
 // // Average Weight Test:
-// let averageWeight = getAverageWeight(sentenceMap);
-// console.log("Average Weight is: " + averageWeight);
+let averageWeight = getAverageWeight(sentenceMap);
+// console.log("Average Weight: " + averageWeight);
 
 // Summarisation Test:
 let summary = summarise(test_text);
