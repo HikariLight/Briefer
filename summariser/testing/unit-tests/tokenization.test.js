716faec315b0215ef5063fd66fcc106b50ca6e48
Dhia M <merzougui.dhia@gmail.com>
2022-04-14 10:08:58 +0200

    Changed import variable's name

--- summariser/testing/unit-tests/tokenization.test.js
+++ summariser/testing/unit-tests/tokenization.test.js
@@ -1,28 +1,28 @@
-import * as Tkz from "../../tokenization.js";
+import * as tokenizer from "../../tokenization.js";
 
 describe("Word Tokenization", () => {
 
-    it("Case: Normal", () => {
-        expect(Tkz.tokenizeWords("Hello, my name is HikariLight. I'm implementing Unit Testing.")).toEqual(["Hello,", "my", "name", "is", "HikariLight.", "I'm", "implementing", "Unit", "Testing."]);
+    test("Case: Normal", () => {
+        expect(tokenizer.tokenizeWords("Hello, my name is HikariLight. I'm implementing Unit Testing.")).toEqual(["Hello,", "my", "name", "is", "HikariLight.", "I'm", "implementing", "Unit", "Testing."]);
     })
 
-    it("Case: dash in text", () => {
-        expect(Tkz.tokenizeWords("Hello, my name is Hikari-Light. I'm implementing Unit-Testing.")).toEqual(["Hello,", "my", "name", "is", "Hikari", "Light.", "I'm", "implementing", "Unit", "Testing."]);
+    test("Case: dash in text", () => {
+        expect(tokenizer.tokenizeWords("Hello, my name is Hikari-Light. I'm implementing Unit-Testing.")).toEqual(["Hello,", "my", "name", "is", "Hikari", "Light.", "I'm", "implementing", "Unit", "Testing."]);
     })
 
 })
 
 describe("Sentence Tokenization", () => {
 
-    it("Case: Normal", () => {
-        expect(Tkz.tokenizeSentences("Hello, my name is HikariLight. I'm implementing Unit Testing.")).toEqual([["Hello,", "my", "name", "is", "HikariLight"], ["I'm", "implementing", "Unit", "Testing"]]);
+    test("Case: Normal", () => {
+        expect(tokenizer.tokenizeSentences("Hello, my name is HikariLight. I'm implementing Unit Testing.")).toEqual([["Hello,", "my", "name", "is", "HikariLight"], ["I'm", "implementing", "Unit", "Testing"]]);
     })
 
-    it("Case: Edge case (Mr.)", () => {
-        expect(Tkz.tokenizeSentences("Hello, my name is Mr. HikariLight. I'm implementing Unit Testing.")).toEqual([["Hello,", "my", "name", "is", "Mr", "HikariLight"], ["I'm", "implementing", "Unit", "Testing"]]);
+    test("Case: Edge case (Mr.)", () => {
+        expect(tokenizer.tokenizeSentences("Hello, my name is Mr. HikariLight. I'm implementing Unit Testing.")).toEqual([["Hello,", "my", "name", "is", "Mr", "HikariLight"], ["I'm", "implementing", "Unit", "Testing"]]);
     })
 
-    it("Case: Edge case large number with period separator", () => {
-        expect(Tkz.tokenizeSentences("I heard that 9.000 is a famous number online. It has something to do with Dragon Ball Z.")).toEqual([["I", "heard", "that", "9000", "is", "a", "famous", "number", "online"], ["It", "has", "something", "to", "do", "with", "Dragon", "Ball", "Z"]]);
+    test("Case: Edge case large number with period separator", () => {
+        expect(tokenizer.tokenizeSentences("I heard that 9.000 is a famous number online. It has something to do with Dragon Ball Z.")).toEqual([["I", "heard", "that", "9000", "is", "a", "famous", "number", "online"], ["It", "has", "something", "to", "do", "with", "Dragon", "Ball", "Z"]]);
     })
 })
